054c96f6b4c39e3c3b8251fbf075a4714573b558
vyava <zafergenc02@gmail.com>
2022-01-14 01:18:02 +0300

    input placeholder color fixed. map sidebar margin problem solved

--- app/src/components/map/search/index.tsx
+++ app/src/components/map/search/index.tsx
@@ -10,6 +10,18 @@ const MapSearch = () => {
 
     useEffect(() => {
         mapRef.current = L.map('map', { zoomControl: true, center: [51.505, -0.09], zoom: 13 });
+        
+
+        var littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.'),
+        denver    = L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.'),
+        aurora    = L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.'),
+        golden    = L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.');
+
+        var cities = L.layerGroup([littleton, denver, aurora, golden]);
+
+        mapRef.current.addLayer(cities);
+
+
     }, [])
 
     return (
