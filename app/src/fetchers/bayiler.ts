b83d701773935e0fe631dd34bf42653a5dd10afe
vyava <zafergenc02@gmail.com>
2022-01-31 14:46:45 +0100

    improvement

--- app/src/fetchers/bayiler.ts
+++ app/src/fetchers/bayiler.ts
@@ -1,4 +1,9 @@
+import { AxiosError } from "axios";
+import { useQuery, UseQueryOptions, UseQueryResult } from "react-query";
+import { IBayi } from "src/interfaces/bayi.interface";
 import BayiService from "src/services/bayi";
+
+const defaultQueryOptions: Pick<UseQueryOptions, 'refetchOnWindowFocus'> = { refetchOnWindowFocus: false };
 
 export const bayiFetcher = async ({queryKey} : any) => {
     const [key, query] = queryKey;
@@ -7,4 +12,12 @@ export const bayiFetcher = async ({queryKey} : any) => {
     }else if(key == 'bayilerJson'){
         return BayiService.getBayiler(query);
     }
+};
+
+export const useBayiler = (): UseQueryResult<IBayi[], AxiosError>  => {
+    return useQuery<IBayi[], AxiosError>(
+        ['bayiler'],
+        () => BayiService.getBayiler({}),
+        defaultQueryOptions
+    );
 }
--- app/src/fetchers/pings.ts
+++ app/src/fetchers/pings.ts
@@ -2,16 +2,33 @@ import BayiService from "src/services/bayi";
 
 import { AxiosError } from "axios";
 import { useQuery, UseQueryResult } from "react-query";
-import { ITelemetryPoint } from "src/types/map";
+import { IBayiPoint } from "src/types/map";
+import { getPositionFromCoords } from "src/components/map/map_helpers";
 
 /**
    *
    */
-const usePings = (start?: string, end?: string): UseQueryResult<ITelemetryPoint[], AxiosError> => {
-    return useQuery<ITelemetryPoint[], AxiosError>(
+const usePings = (start?: string, end?: string): UseQueryResult<IBayiPoint[], AxiosError> => {
+    return useQuery<IBayiPoint[], AxiosError>(
         ['pings', { start, end }],
-        () => BayiService.getBayiler({}),
-        // defaultQueryOptions
+        async () => {
+            let bayiler = await BayiService.getBayiler({});
+            let pings = bayiler.map(bayi => {
+                let { coords, ruhsatNo, ...rest  } = bayi;
+
+                return {
+                    type : 'Feature',
+                    geometry : {
+                        type : 'Point',
+                        coordinates : getPositionFromCoords(coords)
+                    },
+                    id : ruhsatNo,
+                    coords : coords,
+                    properties : rest
+                } as IBayiPoint
+            });
+            return pings
+        }
     );
 };
 
