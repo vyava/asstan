1daae4c89e02edc7ed032e319ef9c96dc550a880
vyava <zafergenc02@gmail.com>
2022-02-10 14:43:49 +0100

    req body solved

--- api/src/api/controllers/town.controller.ts
+++ api/src/api/controllers/town.controller.ts
@@ -4,7 +4,7 @@ import { Town } from '../models/town.model';
 
 export async function getTownsByCityNames(req: Request, res: Response, next: NextFunction) {
     console.log(req.body)
-    let cities = [].concat(req.body.query);
+    let cities = [].concat(req.body);
     try {
         let citiesResult = await Town.getTownsByCityNames(cities);
         res.json(citiesResult)
