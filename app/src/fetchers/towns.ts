d7ede708f27fbd49770d580a93692c8c5185be5c
vyava <zafergenc02@gmail.com>
2022-02-10 14:43:02 +0100

    towns fetcher fixed

--- app/src/fetchers/towns.ts
+++ app/src/fetchers/towns.ts
@@ -16,20 +16,24 @@ const useTowns = ( query : ITownQuery | ITownQuery[], options): UseQueryResult<I
         ['towns', townQuery],
         async () => {
             let towns = await TownsService.getTowns(townQuery);
-            let townPolygons = towns.map(town => {
-                let { _id, geolocation : { polygons, lat, lng }, ...rest  } = town;
-
-                return {
-                    type: 'Feature',
-                    geometry: {
-                        type : 'Polygon',
-                        coordinates : polygons
-                    },
-                    id: _id,
-                    properties: {lat, lng, ...rest}
-                } as ITownLine
-            });
-            return townPolygons
+            if(!!towns && towns.length > 0) {
+                let townPolygons = towns.map(town => {
+                    let { _id, geolocation : { polygons, lat, lng }, ...rest  } = town;
+    
+                    return {
+                        type: 'Feature',
+                        geometry: {
+                            type : 'Polygon',
+                            coordinates : polygons
+                        },
+                        id: _id,
+                        properties: {lat, lng, ...rest}
+                    } as ITownLine
+                });
+                return townPolygons
+            }else{
+                console.log("towns yok")
+            }
         },
         options
     );
