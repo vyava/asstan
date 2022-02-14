0788932eab4933469bfca4ac3ea74a8b8b041e29
vyava <zafergenc02@gmail.com>
2022-02-14 20:55:47 +0100

    wrapper div removed

--- app/src/components/map/sidebar/index.tsx
+++ app/src/components/map/sidebar/index.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { useQuery } from "react-query";
 import List from "src/components/list";
+import { MapFilter } from "src/components/mapFilter";
 import { bayiFetcher } from "src/fetchers/bayiler";
 import styles from "./sidebar.module.scss";
 
@@ -16,15 +17,14 @@ const Sidebar = () => {
 
     return (
         <div className={styles.root}>
+            <MapFilter />
             <div className={styles.heading}>
                 <h2>Arama Sonuçları :</h2>
                 <b>30 Bayi</b>
             </div>
-            <div>
-                <List pagination={false}>
-                    <List.Body rows={data} />
-                </List>
-            </div>
+            <List pagination={false}>
+                <List.Body rows={data} />
+            </List>
         </div>
     )
 };
