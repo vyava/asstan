eadae310b09b43907ee021ca461508514b36add6
vyava <zafergenc02@gmail.com>
2022-03-09 21:06:20 +0100

    state sharing between pages has been solved

--- app/src/contexts/map.context.tsx
+++ app/src/contexts/map.context.tsx
@@ -12,17 +12,14 @@ const defaultIsStale = false;
 const defaultDistricts = useDistricts();
 
 export type MapContextType = {
-    isStale : boolean;
+    groupedDistricts: any;
+    defaultOptions?: any[];
 
-    filters : FiltersType;
-    groupedDistricts : any;
-    defaultOptions? : any[];
+    setIsStale: (value: boolean) => void;
+    setFilters: (value: any[]) => void;
 
-    setIsStale : (value : boolean) => void;
-    setFilters : (value : any[]) => void;
-
-    useBayiler : () => UseQueryResult<IBayi[], AxiosError>;
-    usePagination : () => UseQueryResult<PaginatorType>;
+    useBayiler: () => UseQueryResult<IBayi[], AxiosError>;
+    usePagination: () => UseQueryResult<PaginatorType>;
 };
 
 type FiltersType = {
@@ -41,14 +38,14 @@ const MapContextProvider = ({ children }: any) => {
             result["cities"].push(current.city);
             result["towns"] = result["towns"].concat(current.districts.map(d => d.name));
             return result;
-        }, { cities : [], towns : []});
+        }, { cities: [], towns: [] });
     };
 
     const groupDistricts = (districts) => {
         return districts?.map((district, i) => ({
             id: i,
             label: district.city,
-            value : district.city,
+            value: district.city,
             options: district.districts.map((d, j) => ({
                 city: district.city,
                 label: d.name,
@@ -56,77 +53,76 @@ const MapContextProvider = ({ children }: any) => {
             }))
         }));
     };
-    
+
     const router = useRouter();
-    const { page = "1", limit = 40 } : any = router.query;
+    const { page = "1", limit = 40 }: any = router.query;
 
     const groupedDistricts = groupDistricts(defaultDistricts);
-    const defaultOptions = [].concat.apply([], groupedDistricts?.map(v => v.options));
+    let defaultOptions = [].concat.apply([], groupedDistricts?.map(v => v.options));
+    let { cities : defaultCities, towns : defaultTowns } = setupDistricts(defaultDistricts) || { cities: [], towns: [] };
+    
+    // Set initial filters after route changed to prevent unreliable results
+    useEffect(() => {
+        router.events.on('beforeHistoryChange', (pathname: string) => {
+            if (!pathname.includes("/map")) {
+                setFiltersState({page : 1, limit : 40, cities : defaultCities, towns : defaultTowns});
+                setIsStale(false);
+            }
+        })
+    }, []);
 
     const [isStale, setIsStaleState] = useState(defaultIsStale);
-    
-    let {cities, towns } = setupDistricts(defaultDistricts) || {cities : [], towns : []};
 
-    const [filters, setFiltersState] = useState({page, limit, cities, towns} as FiltersType);
-    
-    
-
-    const setPagination = (pag) => {
-        setPaginatorState(pag);
-    }
+    const [filters, setFiltersState] = useState({ page, limit, cities : defaultCities, towns : defaultTowns } as FiltersType);
 
     useEffect(() => {
-        setFiltersState({...filters, page, limit});
+        setFiltersState({ ...filters, page, limit });
         setIsStale(false);
     }, [page, limit]);
 
-    const setFilters = (values : any[]) => {
-        let {cities, towns } = values.reduce((result, value) => {
+    const setFilters = (values: any[]) => {
+        let { cities, towns } = values.reduce((result, value) => {
             result["cities"].add(value.city)
             result["towns"].add(value.label)
             return result;
-        }, {cities : new Set(), towns : new Set()});
+        }, { cities: new Set(), towns: new Set() });
 
-        setFiltersState({...filters, cities : Array.from(cities), towns : Array.from(towns)});
+        setFiltersState({ ...filters, cities: Array.from(cities), towns: Array.from(towns) });
         setIsStale(false);
     };
 
-    const setIsStale = (value : boolean) => {
+    const setIsStale = (value: boolean) => {
         setIsStaleState(value);
     };
 
-    const useBayiler = () : UseQueryResult<IBayi[], AxiosError> => {
+    // Initial Call
+    useQuery(["bayiler_main", filters], bayiFetcher, {
+        initialData: null,
+        refetchOnWindowFocus: false,
+        enabled: !isStale,
+        notifyOnChangeProps: 'tracked',
+        onSuccess: (result) => {
+            setPagination(result?.paginator);
+            setBayiler(result?.data);
+            setIsStale(true);
+        },
 
-        return useQuery(["bayiler", filters], bayiFetcher, {
-            initialData: null,
-            refetchOnWindowFocus: false,
-            select: res => {
-                setPagination(res?.paginator);
-                return res?.data
-            },
-            enabled : !isStale,
-            onSuccess : (values) => {
-                setIsStale(true);
-            },
+    });
 
-        });
-    };
+    // Bayiler
+    const [bayiler, setBayilerState] = useState([] as IBayi[]);
+    const setBayiler = (bayiler) => setBayilerState(bayiler)
+    const getBayiler = () => bayiler;
+    const useBayiler = (): UseQueryResult<IBayi[], AxiosError> => useQuery(["bayiler"], getBayiler, { enabled: !!isStale });
 
     // Pagination
     const [paginator, setPaginatorState] = useState(null as PaginatorType);
-
-    const getPagination = () => {
-        return paginator;
-    }
-
-    const usePagination = () => {
-        return useQuery(["pagination"], getPagination, {
-            enabled : !!isStale
-        });
-    }
+    const setPagination = (pag) => setPaginatorState(pag);
+    const getPagination = () => paginator;
+    const usePagination = () => useQuery(["pagination"], getPagination, { enabled: !!isStale });
 
     return (
-        <mapContext.Provider value={{ isStale, setIsStale, filters, setFilters, defaultOptions, groupedDistricts, useBayiler, usePagination }}>
+        <mapContext.Provider value={{ setIsStale, defaultOptions, groupedDistricts, useBayiler, usePagination, setFilters }}>
             {children}
         </mapContext.Provider>
     )
--- app/src/pages/map/index.tsx
+++ app/src/pages/map/index.tsx
@@ -1,13 +1,10 @@
 import MainLayout from "src/layouts/Main";
 import Map from "src/components/map";
-import { MapContextProvider } from "src/contexts/map.context";
 
 const MapPage = () => {
   return (
     <MainLayout title="Map Page">
-      <MapContextProvider>
-        <Map />
-      </MapContextProvider>
+      <Map />
     </MainLayout>
   );
 };
